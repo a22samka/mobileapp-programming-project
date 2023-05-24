2e94a4c21651fbb1eb31f490eaa6d523ab2d3301
a22samka <a22samka@student.his.se>
2023-05-24 15:04:31 +0200

    Gjorde en tillbaka knapp som går tillbaks till startsidan.

--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -3,12 +3,24 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button tillbaks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tillbaks = findViewById(R.id.knappTillbaka);
+        tillbaks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
